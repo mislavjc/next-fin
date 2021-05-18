44a153e5397d6f0fba2d3338ccafdd5c6c7a1ebe
Mislav <mislavjc@gmail.com>
2021-05-18 20:38:03 +0200

    protected route from non-logged in users

--- src/pages/setup/index.js
+++ src/pages/setup/index.js
@@ -1,7 +1,17 @@
 import { BasicOptions } from "@/components/setup/BasicOptions";
 import Grid from "@material-ui/core/Grid";
+import { useSession } from "next-auth/client";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Setup() {
+  const [session, loading] = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    if (!session) {
+      router.push("/api/auth/signin");
+    }
+  }, [session, loading]);
 
   return (
     <div className="container">

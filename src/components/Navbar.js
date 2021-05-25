1776c33d9fab1d08a280ac6fa0317e3a69f41204
Mislav <mislavjc@gmail.com>
2021-05-25 13:36:02 +0200

    landing page basic styling + mobile nav added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,103 @@
 import Link from "next/link";
+import { useState } from "react";
 import { signIn, signOut, useSession } from "next-auth/client";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import AccountCircle from "@material-ui/icons/AccountCircle";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuIcon from "@material-ui/icons/Menu";
 
 export const Navbar = () => {
   const [session, loading] = useSession();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <>
       <nav>
-        <img src="/icons/logo.svg" alt="logo" width="40px" height="40px" />
-        <Link href="/">Naslovnica</Link>
-        <Link href="/form">Aplikacija</Link>
-        <Link href="/all-items">Svi</Link>
-        <div className="account">
-          {session && !loading ? (
-            <div>
-              <Button variant="outlined" className="btn" onClick={signOut}>
-                Sign out
+        <img
+          className="logo"
+          src="/icons/logo.svg"
+          alt="logo"
+          width="40px"
+          height="40px"
+        />
+        <div>
+          <img src="/icons/logo.svg" alt="logo" width="40px" height="40px" />
+          <Link href="/">Naslovnica</Link>
+          <Link href="/form">Aplikacija</Link>
+          <Link href="/all-items">Svi</Link>
+          <div className="account">
+            {session && !loading ? (
+              <div>
+                <Button variant="outlined" className="btn" onClick={signOut}>
+                  Sign out
+                </Button>
+                <IconButton
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+              </div>
+            ) : (
+              <Button variant="outlined" className="btn" onClick={signIn}>
+                Sign in
               </Button>
-              <IconButton
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-            </div>
-          ) : (
-            <Button variant="outlined" className="btn" onClick={signIn}>
-              Sign in
-            </Button>
-          )}
+            )}
+          </div>
         </div>
+        <Button
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+          className="nav-menu"
+        >
+          <MenuIcon />
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>
+            <Link href="/">Naslovnica</Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Link href="/form">Aplikacija</Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Link href="/all-items">Svi</Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <div className="account">
+              {session && !loading ? (
+                <div>
+                  <Button variant="outlined" className="btn" onClick={signOut}>
+                    Sign out
+                  </Button>
+                </div>
+              ) : (
+                <Button variant="outlined" className="btn" onClick={signIn}>
+                  Sign in
+                </Button>
+              )}
+            </div>
+          </MenuItem>
+        </Menu>
       </nav>
     </>
   );
-}
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,29 @@
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <div>Homepage</div>
+      <div style={{ background: "#F5F9FF", padding: "2rem 0" }}>
+        <div className="landing-text">
+          <Typography
+            variant="h2"
+            component="h1"
+            align="center"
+            fontWeight={600}
+          >
+            Financije i narudžbenice, pojednostavljene
+          </Typography>
+          <Typography variant="h4" component="h2" align="center">
+            FIN je platforma koja olakšava evidenciju poslovnih procesa,
+            jednostavna je za korištenje te je dostupna bilo kada i bilo gdje.
+          </Typography>
+          <Button variant="contained" size="large" color="primary">
+            <Link href="/setup">Započnite korištenje</Link>
+          </Button>
+        </div>
+      </div>
     </>
   );
 }

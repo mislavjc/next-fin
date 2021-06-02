9e5753426851f0b3a879e57dc263fff0f8d194cc
Mislav <mislav.jovanic@coreline.agency>
2021-06-02 15:12:55 +0200

    card ui changes

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -6,7 +6,6 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import IconButton from "@material-ui/core/IconButton";
 import KeyboardBackspaceIcon from "@material-ui/icons/KeyboardBackspace";
 import ArchiveIcon from "@material-ui/icons/Archive";
@@ -30,54 +29,54 @@ export const CardItem = ({ form, onOpen, onClose, showBack }) => {
   };
 
   return (
-    <Card>
+    <Card onClick={onOpen} className={!showBack ? "card" : null}>
       <CardContent>
         {form.inputs.map((input) => (
           <Typography className="card-content" variant="body1" key={input._id}>
-            {input.type.name}: {input.value}
+            {input.type.name}: <span className="value">{input.value}</span>
           </Typography>
         ))}
       </CardContent>
       <CardActions>
-        {showBack ? (
+        {showBack && (
           <Tooltip title="Nazad">
             <IconButton onClick={onClose}>
               <KeyboardBackspaceIcon />
             </IconButton>
           </Tooltip>
-        ) : (
-          <Tooltip title="Više">
-            <IconButton onClick={onOpen}>
-              <MoreHorizIcon />
-            </IconButton>
-          </Tooltip>
         )}
-        <Tooltip title="Promjeni">
-          <IconButton
-            onClick={() => router.push(`/all-items/${form._id}/edit`)}
-          >
-            <EditIcon />
-          </IconButton>
-        </Tooltip>
         <span style={{ marginLeft: "auto" }}>
-          {form.archived ? (
-            <Tooltip title="Vrati iz arhiva">
-              <IconButton onClick={unArchiveHandler}>
-                <UnarchiveIcon />
-              </IconButton>
-            </Tooltip>
-          ) : (
-            <Tooltip title="Arhiviraj">
-              <IconButton onClick={archiveHandler}>
-                <ArchiveIcon />
-              </IconButton>
-            </Tooltip>
-          )}
-          <Tooltip title="Obriši">
-            <IconButton onClick={deleteHandler}>
-              <DeleteIcon />
+          <Tooltip title="Promjeni" >
+            <IconButton
+              style={{ zIndex: 7 }}
+              onClick={() => router.push(`/all-items/${form._id}/edit`)}
+              className="edit"
+            >
+              <EditIcon className="edit" />
             </IconButton>
           </Tooltip>
+          {showBack && (
+            <>
+              {form.archived ? (
+                <Tooltip title="Vrati iz arhiva">
+                  <IconButton onClick={unArchiveHandler}>
+                    <UnarchiveIcon />
+                  </IconButton>
+                </Tooltip>
+              ) : (
+                <Tooltip title="Arhiviraj">
+                  <IconButton onClick={archiveHandler}>
+                    <ArchiveIcon />
+                  </IconButton>
+                </Tooltip>
+              )}
+              <Tooltip title="Obriši">
+                <IconButton onClick={deleteHandler}>
+                  <DeleteIcon />
+                </IconButton>
+              </Tooltip>
+            </>
+          )}
         </span>
       </CardActions>
     </Card>

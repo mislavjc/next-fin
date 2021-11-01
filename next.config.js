4a0feadd7c31142e5a87e7c3b752d9bc04d7e4bc
Mislav <acj@accointing.com>
2021-11-01 19:06:29 +0100

    PWA support!

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
   i18n: {
     locales: ['hr', 'en'],
     defaultLocale: 'hr',
   },
-};
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+  },
+});

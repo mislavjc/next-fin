d3b1175a90168d77ddb7e1557cb669766deb3851
Mislav <mislavjc@gmail.com>
2022-04-13 11:42:51 +0200

    add clg

--- src/lib/filter.js
+++ src/lib/filter.js
@@ -1,6 +1,8 @@
 export const mapAndReduce = (entries) => {
+  console.log('...mapping entries')
   const mappedEntries = entries
     .map((entry) => entry.inputs.map((input) => input?.value))
     .flat();
+  console.log([...new Set(mappedEntries)]);
   return [...new Set(mappedEntries)];
 };
